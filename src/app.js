d0960191b3576dda2400769abe0cc6a99abd7637
loconluis <luis.locon@gmail.com>
2017-11-06 20:11:02 -0600

    First commit for expensify

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Styles 
+import 'normalize.css/normalize.css'; // normalizing styles for all browsers
+import './styles/style.scss';
+
+
+ReactDOM.render(<h2>I'm a boilerplate</h2>, document.getElementById('app'))
