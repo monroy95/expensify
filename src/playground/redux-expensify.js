e4a34c21db7cbb78c74a0d9d8bc014767be30bb3
loconluis <luis.locon@gmail.com>
2017-11-08 00:06:07 -0600

    Demo state to works with complex redux example

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,1 +1,18 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: 'ksjdiajdioe21',
+    description: 'January Rent',
+    notes: 'This was the final payment for that address',
+    amout: 54500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amout', // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
+
