eb12e64cbbaf437b2bd484223279313f55c6b077
loconluis <luis.locon@gmail.com>
2017-11-09 15:58:49 -0600

    Connecting with redux devtool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,8 @@ export default () => {
   const store = createStore(combineReducers({
     expenses: expensesReducer,
     filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // to connect with redux dev tool
   );
 
   return store;
