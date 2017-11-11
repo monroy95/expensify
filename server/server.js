960860873610facd33dad7a9e2500c31296ba10f
loconluis <luis.locon@gmail.com>
2017-11-11 00:50:03 -0600

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..' ,'public')
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 })
 
-app.listen(3000, () => {
-  console.log(`App is served in http://localhost:3000/`);
+app.listen(port, () => {
+  console.log(`App is served in http://localhost:${PORT}/`);
 });
