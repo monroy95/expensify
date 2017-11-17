c3c9291c5335fbca11ca102e0e0d164a26d7987d
loconluis <luis.locon@gmail.com>
2017-11-16 23:54:46 -0600

    Some modifies for components and adding style to login page and header layout

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 
 import expensesTotal from '../selectors/expenses-total';
@@ -7,8 +8,13 @@ import selectExpenses from '../selectors/expenses';
 
 export const ExpensesSummary = ({ expensesCount, total }) => {
   return (
-    <div>
-      <h1>Viewing { expensesCount } totalling { numeral(total / 100).format('$0,0.00')  } </h1> 
+    <div className="page-header">
+      <div className="container">
+        <h1 className="page-header__title">Viewing <span>{ expensesCount }</span> totalling <span>{ numeral(total / 100).format('$0,0.00')  }</span> </h1> 
+        <div className="page-header__actions">
+          <Link to="/create" className="btn">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-  <button onClick={startLogin}>Log in with Google Auth</button>
-</div>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify App</h1>
+      <p>It's time to get your expenses under control.</p>
+      <button className="btn" onClick={startLogin}>Log in with <i className="fa fa-google-plus" aria-hidden="true"></i></button>
+    </div>
+  </div>
 )
 
 const mapDispatchToProps = (dispatch) => ({
