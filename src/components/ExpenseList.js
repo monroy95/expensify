5c6668839b935d9ec9316c862b4a9323336d3152
loconluis <luis.locon@gmail.com>
2017-11-17 01:17:35 -0600

    Styling list of expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -9,10 +9,17 @@ import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => {
   return (
-    <div>
-      {
-        props.expenses.length === 0 ? (<p>No Expenses</p>) : (props.expenses.map((expense, index) =><ExpenseListItem key={index} {...expense} />))
-      }
+    <div className="container">
+      <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+      </div>
+      <div className="list-body">
+        {
+          props.expenses.length === 0 ? (<div className="list-item list-item--empty"><span>No Expenses</span></div>) : (props.expenses.map((expense, index) =><ExpenseListItem key={index} {...expense} />))
+        }
+      </div>
     </div>
   )
 }
