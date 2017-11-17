167d3c659ab2d7ae0bd2d9ec835fbe0a869293e9
loconluis <luis.locon@gmail.com>
2017-11-17 00:30:16 -0600

    Styling inputs

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -75,6 +75,7 @@ export default class ExpenseForm extends React.Component {
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
+            className="text-input"
             type="text"
             placeholder="Description"
             autoFocus
@@ -82,6 +83,7 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onDescriptionChange}
           />
           <input
+            className="text-input"
             type="text"
             placeholder="Amount"
             value={this.state.amount}
@@ -96,6 +98,7 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false }
           />
           <textarea
+            className="textarea"
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
